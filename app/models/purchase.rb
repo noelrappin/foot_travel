5a3f01088ed5165b8c5679430bc5aef31addcb62
Noel Rappin <noel@groupon.com>
2012-03-16 22:52:22 -0600

    friend stuff

--- app/models/purchase.rb
+++ app/models/purchase.rb
@@ -1,6 +1,5 @@
 class Purchase < ActiveRecord::Base
   include LineItemAggregator
-  attr_accessor :lodging
 
   belongs_to :buyer, :polymorphic => true
   belongs_to :purchasable, :polymorphic => true
